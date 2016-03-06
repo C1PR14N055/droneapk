eb5a183d74506cfc3e891f7227ce5f90836427a4
C1PR14N055 <ciprianosss@gmail.com>
2016-03-06 12:16:50 +0200

    added wifi controller

---
+++ app/src/main/java/ro/drone/ciprian/droneapp/Controller.java
@@ -0,0 +1,31 @@
+package ro.drone.ciprian.droneapp;
+
+import android.view.InputDevice;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ciprian on 3/6/16.
+ */
+public class Controller {
+
+    public static ArrayList getGameControllerIds() {
+        ArrayList gameControllerDeviceIds = new ArrayList();
+        int[] deviceIds = InputDevice.getDeviceIds();
+        for (int deviceId : deviceIds) {
+            InputDevice dev = InputDevice.getDevice(deviceId);
+            int sources = dev.getSources();
+
+            // Verify that the device has gamepad buttons, control sticks, or both.
+            if (((sources & InputDevice.SOURCE_GAMEPAD) == InputDevice.SOURCE_GAMEPAD)
+                    || ((sources & InputDevice.SOURCE_JOYSTICK) == InputDevice.SOURCE_JOYSTICK)) {
+                // This device is a game controller. Store its device ID.
+                if (!gameControllerDeviceIds.contains(deviceId)) {
+                    gameControllerDeviceIds.add(deviceId);
+                }
+            }
+        }
+        return gameControllerDeviceIds;
+    }
+
+}
--- app/src/main/java/ro/drone/ciprian/droneapp/MainActivity.java
+++ app/src/main/java/ro/drone/ciprian/droneapp/MainActivity.java
@@ -3,11 +3,15 @@ package ro.drone.ciprian.droneapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.InputDevice;
+import android.view.KeyEvent;
+import android.view.WindowManager;
 import android.widget.SeekBar;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,23 +24,29 @@ public class MainActivity extends AppCompatActivity {
     String messageStr;
     boolean sendStuff = true;
 
+    ArrayList gameControllerDevicesIds;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        gameControllerDevicesIds = Controller.getGameControllerIds();
 
         new Thread(new Runnable() {
             public void run() {
                 try {
                     DatagramSocket s = new DatagramSocket();
-                    InetAddress local = InetAddress.getByName("192.168.1.143");
+                    InetAddress local = InetAddress.getByName("192.168.1.87");
                     while (sendStuff) {
                         messageStr = String.valueOf((roll.getProgress() + 1000) + "" + (pitch.getProgress() + 1000) +
                                 (yaw.getProgress() + 1000) + (throttle.getProgress() + 1000));
                         byte[] message = messageStr.getBytes();
                         DatagramPacket p = new DatagramPacket(message, messageStr.length(), local, SERVER_PORT);
                         s.send(p);
-                        Thread.sleep(5);
+                        Thread.sleep(10);
                     }
 
                 } catch (Exception ex) {
@@ -117,6 +127,15 @@ public class MainActivity extends AppCompatActivity {
                 seekBar.setProgress(seekBar.getMax() / 2);
             }
         });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((event.getSource() & InputDevice.SOURCE_GAMEPAD) == InputDevice.SOURCE_GAMEPAD) {
+            Log.d("INPUT KEYCODE", String.valueOf(keyCode) + " FROM: " + event.getDeviceId());
+            return true; // if handled
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
 }
