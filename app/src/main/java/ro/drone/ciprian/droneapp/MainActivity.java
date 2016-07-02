d5597a05d5df480f65e0346702fee8a84bd24751
C1PR14N055 <ciprianosss@gmail.com>
2016-07-02 15:12:31 +0300

    TCP

--- app/src/main/java/ro/drone/ciprian/droneapp/MainActivity.java
+++ app/src/main/java/ro/drone/ciprian/droneapp/MainActivity.java
@@ -31,10 +31,18 @@ import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -348,8 +356,23 @@ public class MainActivity extends AppCompatActivity {
         runnable.run();
 
         // UDP THREAD
-        Runnable runableUDP = new Runnable() {
+        Thread runableUDP = new Thread() {
             public void run() {
+
+                try {
+                    InetAddress serverAddress = InetAddress.getByName("10.0.3.2");
+                    Socket socket = new Socket(serverAddress, 21);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+                /*
                 try {
                     DatagramSocket s = new DatagramSocket();
                     InetAddress local = InetAddress.getByName(localPiIP);
@@ -386,8 +409,11 @@ public class MainActivity extends AppCompatActivity {
                 } catch (Exception ex) {
                     Log.e("err", ex.getMessage());
                 }
+                */
             }
         };
+
+        runableUDP.start();
     }
 
     @Override
